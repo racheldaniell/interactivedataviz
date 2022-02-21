621bbd6aabc8b3ee5f96061b3a1c56a5b4929550
rdaniell <rdaniell@pratt.edu>
2022-02-21 15:40:36 -0500

    partial coding from class03

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,17 +1,47 @@
-/* CONSTANTS AND GLOBALS */
-// const width = ;
-// const height = ;
+// 1 VARIABLES
+// we define variables/globals we will use throughout
 
-/* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
-  .then(data => {
-    console.log("data", data)
+const width = window.innerWidth *.08;
+const height = window.innerHeight /3;
 
-    /* SCALES */
-    /** This is where you should define your scales from data to pixel space */
-    
 
-    /* HTML ELEMENTS */
-    /** Select your container and append the visual elements to it */
+// 2 DATA
+// we have d3 load in our CSV file using "d3.csv"
+// we have d3 guess our data types using "d3.autotype"
+// we output that data to the console using console.log
 
-  })
+d3.csv('../data/squirrelActivities.csv', d3.autoType).then(data => {
+  console.log("data", data)
+
+
+// 3 SCALES
+// domain relates to data
+// range relates to visuals
+// we are using "d=> d.____" to tell it to get the value for each row in the column specified
+// the data domain for numbers needs to use a max number using "d3.max"
+
+const xScale = d3.scaleBand()
+.domain(data.map(d=> d.activity))
+.range([0, width])
+
+
+const yScale = d3.scaleLinear()
+.domain([0, d3.max(data, d=> d.count)]) 
+.range([height, 0])  
+
+
+
+// 4 ELEMENTS APPEND
+
+
+// 5 SELECT - JOIN - DRAW
+
+
+// 6 ATTRIBUTES
+
+
+
+// everything with our data needs to happen within the brackets!
+// these end brackets represent the end of data output area
+
+})
