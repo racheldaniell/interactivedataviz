e25a3cce10e43f60a43f2cdf8efda251e4b7ecb9
rdaniell <rdaniell@pratt.edu>
2022-03-10 17:17:19 -0500

    additions made during class Mar09

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,57 +1,71 @@
-/* 1 - VARIABLES - CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.7,
-  height = window.innerHeight * 0.7,
-  margin = 50;
+/* CONSTANTS AND GLOBALS */
+const width = window.innerWidth * 0.8,
+height = window.innerHeight * 0.8,
+margin = {top: 20, bottom: 35, left: 30, right: 10};
 
 
-/* 2 - DATA - LOAD DATA */
-
+/* LOAD DATA */
 d3.json("../data/environmentRatings.json", d3.autoType)
-  .then(data => {
-    console.log(data)
+.then(data => {
+ console.log(data)
 
-    /* 3 - SCALES */
-    const xScale = d3.scaleLinear()
-      .domain([0,d3.max(data, d => d.envScore2020)])
-      .range([margin,width-margin])
+ /* SCALES */
 
-    const yScale = d3.scaleLinear()
-      .domain([0,d3.max(data, d => d.ideologyScore2020)])
-      .range([height-margin,margin])
+ const xScale = d3.scaleLinear()
+   .domain([0,d3.max(data, d => d.envScore2020)])
+   .range([margin.left, width-margin.right])
 
-    const colorScale = d3.scaleOrdinal()
-      .domain(["R","D","I"])
-      .range(["red","blue","purple"])    
-    /* 4 - ELEMENTS TO APPEND - HTML ELEMENTS */
+ const yScale = d3.scaleLinear()
+   .domain([0,d3.max(data, d => d.ideologyScore2020)])
+   .range([height-margin.bottom,margin.top])
 
-    const svg = d3.select("#container")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
+ const colorScale = d3.scaleOrdinal()
+   .domain(["R","D","I"])
+   .range(["red","blue","purple"])
 
-    const xAxis = d3.axisBottom(xScale)
-    const yAxis = d3.axisLeft(yScale)
+ /* ELEMENTS */
 
-    svg.append("g")
-      .attr("transform", `translate(0,${height-margin})`)
-      .call(xAxis)
+ const svg = d3.select("#container")
+   .append("svg")
+   .attr("width", width)
+   .attr("height", height)
 
-    svg.append("g")
-      .attr("transform",`translate(0,${margin}`)
-      .call(yAxis)
+ const xAxis = d3.axisBottom(xScale)
+ const yAxis = d3.axisLeft(yScale)
+
+ svg.append("g")
+   .attr("transform", `translate(0,${height-margin.bottom})`)
+   .call(xAxis)
+
+ svg.append("g")
+   .attr("transform", `translate(${margin.left},0)`)
+   .call(yAxis)
+
+ /* SELECTALL-DATA-JOIN-DRAW */
+ /* +ATTRIBUTES */
 
     /* 5 - JOIN - SELECT-DATA-JOIN & DRAW */
      /* 6 - ATTRIBUTES TO FINISH DRAWING GRAPHICS */
 
     svg.selectAll("circle")
       .data(data)
-      .join("circle")
-      .attr("cx", d => xScale(d.envScore2020))
-      .attr("cy", d => yScale(d.ideologyScore2020))
-      .attr("r", 3)
-      //.attr("fill", "purple")
-      .attr("fill", d=> colorScale(d.Party))
- 
+      .join(
+        enter => enter
+          .append("circle")
+          .attr("r", 1)
+          .attr("cx", d => xScale(d.envScore2020))
+          .attr("cy", d => yScale(d.ideologyScore2020))
+          .attr("fill", "black")
+            .transition()
+            .duration(4000) // in ms
+            .delay(200)
+              .attr("r", 10)
+              .attr("fill", d=> colorScale(d.Party))
+
+
+      )
+        
+
 
     
   });
--- 2_4_geographic/main.js
+++ 2_4_geographic/main.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
-// const width = window.innerWidth * 0.9,
-//   height = window.innerHeight * 0.7,
-//   margin = { top: 20, bottom: 50, left: 60, right: 40 };
+const width = window.innerWidth * 0.9,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
 /**
  * LOAD DATA
@@ -12,15 +12,82 @@ Promise.all([
   d3.csv("../data/stateCapitals.csv", d3.autoType),
 ]).then(([geojson, capitals]) => {
   
-  // SPECIFY PROJECTION
- 
+  //  IN SCALES AREA SPECIFY PROJECTION as scale
+ const projection = d3.geoAlbersUsa()
+  .fitSize([width - margin.left - margin.right, height - margin.top - margin.bottom], geojson)
 
-  // DEFINE PATH FUNCTION
+  // CREATE SVG ELEMENT
+const svg = d3.select("#container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+
+  // extra added after class: chart label
+  svg.append("text")      
+  .attr("x",  width *.5 )
+  .attr("y",  height-margin.bottom)
+  .style("text-anchor", "middle")
+  .text("Geo Points & Single Point Transition");
+  
+  // PREPARE TO JOIN DATA + DRAW GEO OUTLINES
+  // DEFINE PATH FUNCTION TO DRAW LINES
+const pathGen = d3.geoPath(projection)
+
+  // SELECTALL-DATA-JOIN
+  // for TWO data sets this time, so 2 JOINS
+const states = 
+svg.selectAll("path")
+  .data(geojson.features)
+  .join("path")
+  .attr("d", d => pathGen(d))
+  .attr("fill", "magenta")
+  .attr("stroke", "black")
+
+svg.selectAll("circle")
+  .data(capitals)
+  .join("circle")
+  .attr("r", 3) 
+  .attr("fill", "gold")
+  .attr("transform", d => {
+    const [x, y] = projection([d.longitude, d.latitude])
+    return `translate(${x}, ${y})`
+  })
 
 
+  // First DRAW GEOJSON PATH via SELECT-DATA-JOIN-ATTR for US States
   // APPEND GEOJSON PATH  
   
   
-  // APPEND DATA AS SHAPE
+  // SELECT-DATA-JOIN-DRAW-ATTR to APPEND DATA AS SHAPE
+
+   // extra added after class: single manual point with TRANSITION on enter
+  // draw a point directly through code
+  // draw point for CUNY graduate center
+  // add transition on entry of drawn object to highlight it visually
+  // nonte the example below was added after class for reference for projects
+
+  const gradCenterPoint =  { latitude: 40.7423, longitude: -73.9833 };
+
+  svg.selectAll("circle.point")
+    .data([gradCenterPoint])
+    .join(enter => enter
+      .append("circle")
+      .attr("class", "point")
+      .attr("r", 30)
+         // use our projection to go from long, lat => x, y
+        // note LONG is X and LAT is Y
+        // ref: https://github.com/d3/d3-geo#_projection
+      .attr("transform", d=> {
+        const [x, y] = projection([d.longitude, d.latitude])
+        return `translate(${x}, ${y})`
+      })
+      .attr("fill", "deepskyblue")
+      .call(enter => enter.transition()
+          .delay(200) // in ms
+          .duration(5000)  //in ms
+          .attr("r", 10)
+          .attr("fill", "blue")
+          )
+    )
 
 });
