7b28043fff0170b3a77d69f99ede703b889eb563
rdaniell <rdaniell@pratt.edu>
2022-04-06 17:29:07 -0400

    refresh starting empty js file

---
+++ 4_1_geographic_map/main.js
@@ -0,0 +1,26 @@
+
+// FIRST Set up initial map center and zoom level
+// relatively central coordinates for NYC: 40.70748615030739, -73.92744598608154
+// zoom levels get closer the higher the number - try 11 or 12 or 13 to start
+
+
+
+
+/*
+Later in process: Add a legend (checkboxes) to the upper-right corner.
+At first, baselayers and overlays are set to `null` (empty legend).
+We will be adding items to the legend as we load each layer.
+*/  
+
+    
+// display basemap tiles 
+
+
+
+// see more basemap options at https://leaflet-extras.github.io/leaflet-providers/preview/
+
+
+// read data from data files
+
+
+// add data overlay layers
